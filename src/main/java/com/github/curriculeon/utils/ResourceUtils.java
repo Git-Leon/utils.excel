c52741af42ad8650feda85e05f2ed9327c71047f
Leon Hunter <xleonhunter@gmail.com>
2020-01-25 12:28:28 -0500

    restructured mockfile creation

--- src/main/java/com/github/curriculeon/utils/ResourceUtils.java
+++ src/main/java/com/github/curriculeon/utils/ResourceUtils.java
@@ -20,17 +20,24 @@ public class ResourceUtils {
         return new File(getResourceDirectoryPath() + fileName);
     }
 
-    public static File duplicateFile(String fileName) {
+    public static File loadDuplicate(String fileName) {
         File source = ResourceUtils.getResourceFile(fileName);
         File destination;
+        Long id = System.nanoTime();
+        String parentDirectory = source.getParentFile().getAbsolutePath() + "/";
+        String newFileName = source.getName();
+        Integer lastPeriod = newFileName.lastIndexOf('.');
+        String fileExtension = newFileName.substring(lastPeriod);
+        newFileName = newFileName.substring(0, lastPeriod) + id + "." + fileExtension;
+        destination = new File(parentDirectory + newFileName);
+
+        return destination;
+    }
+
+    public static File duplicateFile(String fileName) {
+        File source = ResourceUtils.getResourceFile(fileName);
+        File destination = loadDuplicate(fileName);
         try {
-            Long id = System.nanoTime();
-            String parentDirectory = source.getParentFile().getAbsolutePath() + "/";
-            String newFileName = source.getName();
-            Integer lastPeriod = newFileName.lastIndexOf('.');
-            String fileExtension = newFileName.substring(lastPeriod);
-            newFileName = newFileName.substring(0, lastPeriod) + id + "." + fileExtension;
-            destination = new File(parentDirectory + newFileName);
             FileUtils.copyFile(source, destination);
         } catch (IOException e) {
             throw new Error(e);
